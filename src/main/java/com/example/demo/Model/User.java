f67f99e2a8e1d0bffdb8672fa654b44aba17810e
Sulthonhanan <hananakhnaf31@gmail.com>
2026-04-14 11:37:08 +0700

    User Class

---
+++ src/main/java/com/example/demo/Model/User.java
@@ -0,0 +1,30 @@
+package com.example.demo.Model;
+public class User {
+    private String nama;
+    private String nim;
+    private String jenisKelamin;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+}
